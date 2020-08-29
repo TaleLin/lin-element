52b46f972fcdaf7ab2e1c9ff9b264cf9de8b6dd2
liulu <1021283712@qq.com>
2020-08-29 22:40:57 +0800

    feat(input-number): init

--- packages/input-number/src/input-number.vue
+++ packages/input-number/src/input-number.vue
@@ -26,15 +26,25 @@
     </span>
     <el-input
       ref="input"
+      :modelValue="displayValue"
+      :placeholder="placeholder"
       :disabled="inputNumberDisabled"
-      :size="inputNumberSize">
+      :size="inputNumberSize"
+      :max="max"
+      :min="min"
+      :name="name"
+      :label="label"
+      @blur="handleBlur"
+      @focus="handleFocus"
+      @input="handleInput"
+      @change="handleInputChange">
     </el-input>
 </div>
 </template>
 
 <script>
 import {
-  computed, getCurrentInstance, inject, ref, reactive
+  computed, getCurrentInstance, inject, ref, reactive, watch
 } from 'vue';
 import ElInput from 'element-ui/packages/input';
 import RepeatClick from 'element-ui/src/directives/repeat-click';
@@ -45,8 +55,25 @@ export default {
     ElInput
   },
   props: {
-    size: String,
+    step: {
+      type: Number,
+      default: 1
+    },
+    stepStrictly: {
+      type: Boolean,
+      default: false
+    },
+    max: {
+      type: Number,
+      default: Infinity
+    },
+    min: {
+      type: Number,
+      default: -Infinity
+    },
+    modelValue: {},
     disabled: Boolean,
+    size: String,
     controls: {
       type: Boolean,
       default: true
@@ -55,20 +82,20 @@ export default {
       type: String,
       default: ''
     },
-    min: {
-      type: Number,
-      default: -Infinity
-    },
-    step: {
-      type: Number,
-      default: 1
-    },
-    value: {},
+    name: String,
+    label: String,
+    placeholder: String,
     precision: {
       type: Number,
       validator(val) {
         return val >= 0 && val === parseInt(val, 10);
       }
+    },
+    onChange: {
+      type: Function
+    },
+    onInput: {
+      type: Function
     }
   },
   directives: {
@@ -79,35 +106,53 @@ export default {
     const elFormItem = inject('elFormItem', '');
     const instance = getCurrentInstance();
     let currentValue = ref(0);
-    let userInput = reactive({});
+    let userInputObj = reactive({'userInput': null});
     const _decrease = (val, step) => {
       if (typeof val !== 'number' && val !== undefined) return currentValue;
-      const precisionFactor = Math.pow(10, numPrecision);
+      const precisionFactor = Math.pow(10, numPrecision.value);
       return toPrecision((precisionFactor * val - precisionFactor * step) / precisionFactor);
     };
     const toPrecision = (num, precision) => {
-      if (precision === undefined) precision = numPrecision;
+      if (precision === undefined) precision = numPrecision.value;
       return parseFloat(Math.round(num * Math.pow(10, precision)) / Math.pow(10, precision));
     };
     const decrease = () => {
-      if (inputNumberDisabled || minDisabled) return;
-      const value = props.value || 0;
+      if (inputNumberDisabled.value || minDisabled.value) return;
+      const value = props.modelValue || 0;
       const newVal = _decrease(value, props.step);
       setCurrentValue(newVal);
     };
     const setCurrentValue = (newVal) => {
       const oldVal = currentValue.value;
       if (typeof newVal === 'number' && props.precision !== undefined) {
-        newVal = props.toPrecision(newVal, props.precision);
+        newVal = toPrecision(newVal, props.precision);
       }
       if (newVal >= props.max) newVal = props.max;
       if (newVal <= props.min) newVal = props.min;
       if (oldVal === newVal) return;
-      userInput = null;
+      userInputObj.userInput = null;
+      ctx.emit('update:modelValue', newVal);
       ctx.emit('input', newVal);
       ctx.emit('change', newVal, oldVal);
       currentValue.value = newVal;
     };
+    let displayValue = computed(() => {
+      if (userInputObj.userInput !== null) {
+        return userInputObj.userInput;
+      }
+      let Value = currentValue.value;
+      if (typeof Value === 'number') {
+        if (props.stepStrictly) {
+          const stepPrecision = getPrecision(props.step);
+          const precisionFactor = Math.pow(10, stepPrecision);
+          Value = Math.round(Value / props.step) * precisionFactor * props.step / precisionFactor;
+        }
+        if (props.precision !== undefined) {
+          Value = Value.toFixed(props.precision);
+        }
+      }
+      return Value;
+    });
     let _elFormItemSize = computed(() => {
       return (elFormItem || {}).elFormItemSize;
     });
@@ -145,22 +190,68 @@ export default {
       return props.controls && props.controlsPosition === 'right';
     });
     const minDisabled = computed(() => {
-      return _decrease(props.value, props.step) < props.min;
+      return _decrease(props.modelValue, props.step) < props.min;
     });
     const maxDisabled = computed(() => {
-      return _increase(props.value, props.step) > props.max;
+      return _increase(props.modelValue, props.step) > props.max;
     });
     const _increase = (val, step) => {
       if (typeof val !== 'number' && val !== undefined) return currentValue;
-      const precisionFactor = Math.pow(10, numPrecision);
+      const precisionFactor = Math.pow(10, numPrecision.value);
       // Solve the accuracy problem of JS decimal calculation by converting the value to integer.
       return toPrecision((precisionFactor * val + precisionFactor * step) / precisionFactor);
     };
     const increase = () => {
-      if (inputNumberDisabled || maxDisabled) return;
-      const value = props.value || 0;
-      const newVal = _increase(value, this.step);
+      if (inputNumberDisabled.value || maxDisabled.value) return;
+      const value = props.modelValue || 0;
+      const newVal = _increase(value, props.step);
       setCurrentValue(newVal);
+    };
+    const useModelWrapper = (props, name = 'modelValue') => {
+      return computed({
+        get: () => props[name],
+        set: (value) => ctx.emit(`update:${name}`, value)
+      });
+    };
+    const value = useModelWrapper(props, 'modelValue');
+    watch(value, (value) => {
+      let newVal = value === undefined ? value : Number(value);
+      if (newVal !== undefined) {
+        if (isNaN(newVal)) {
+          return;
+        }
+        if (props.stepStrictly) {
+          const stepPrecision = getPrecision(props.step);
+          const precisionFactor = Math.pow(10, stepPrecision);
+          newVal = Math.round(newVal / props.step) * precisionFactor * props.step / precisionFactor;
+        }
+
+        if (props.precision !== undefined) {
+          newVal = toPrecision(newVal, props.precision);
+        }
+      }
+      if (newVal >= props.max) newVal = props.max;
+      if (newVal <= props.min) newVal = props.min;
+      currentValue.value = newVal;
+      userInputObj.userInput = null;
+      ctx.emit('input', value);
+    }, {immediate: true});
+    const handleInput = (value) => {
+      ctx.emit('input', value);
+      userInputObj.userInput = value;
+    };
+    const handleBlur = (event) => {
+      ctx.emit('blur', event);
+    };
+    const handleFocus = (event) => {
+      ctx.emit('focus', event);
+    };
+    const handleInputChange = (value) => {
+      const newVal = value === '' ? undefined : Number(value);
+      if (!isNaN(newVal) || value === '') {
+        setCurrentValue(newVal);
+      }
+      userInputObj.userInput = null;
     };
     return {
       inputNumberSize,
@@ -170,7 +261,12 @@ export default {
       maxDisabled,
       increase,
       decrease,
-      userInput
+      userInputObj,
+      displayValue,
+      handleInput,
+      handleBlur,
+      handleFocus,
+      handleInputChange
     };
   }
 };
